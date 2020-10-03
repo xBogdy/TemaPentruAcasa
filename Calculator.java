f8a8b43d5268742584c167ef24fdfc83d9605cd2
Bogdan Lp <mr.niculec@gmail.com>
2020-10-04 01:05:37 +0300

    addedCalculator

---
+++ Calculator.java
@@ -0,0 +1,38 @@
+package com.arrays;
+
+public class Calculator {
+     public static int adunare(int x,int y){
+        int z = x+y;
+         return z;
+    }
+    public static double adunare(double x,double y){
+        double z = x+y;
+        return z;
+    }
+    public static int scadere(int x,int y){
+         int z = x-y;
+    return z;
+     }
+    public static double scadere(double x,double y){
+        double z = x-y;
+    return z;
+     }
+    public static int inmultire(int x,int y){
+        int z =x*y;
+        return z;
+     }
+
+     public static double inmultire(double x,double y){
+        double z =x*y;
+        return z;
+    }
+    public static int impartire(int x,int y){
+         int z=x/y;
+         return z;
+    }
+    public static double impartire(double x,double y){
+         double z=x/y;
+         return z;
+    }
+
+     }
