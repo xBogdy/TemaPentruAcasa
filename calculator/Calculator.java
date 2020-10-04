3fa8acdf124e2631b0cafbd3481aa0f5fab99937
bogdan <mr.niculec@gmail.com>
2020-10-04 19:15:01 +0300

    calculator

---
+++ calculator/Calculator.java
@@ -0,0 +1,22 @@
+package com.step.calculator;
+
+public class Calculator {
+    double x = 13.3;
+    public static void adunare(int x,int y){
+        int z = x+y;
+        System.out.println("Rs:"+z);
+    }
+    public static void scadere(int x,int y){
+        int z = x-y;
+        System.out.println("Rs:"+z);
+    }
+    public static void inmultire(int x,int y){
+        int z =x*y;
+        System.out.println("Rs:"+z);
+    }
+    public static void impartire(int x, int y){
+        int z=x/y;
+        System.out.println("Rs:"+z);
+    }
+    }
+
